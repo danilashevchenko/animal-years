1c88a889af9734ccb6c95efaca7a4d0ed9040dbc
danilashevchenko <danila.shevchenko@yahoo.com>
2023-04-09 21:40:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// My age 
+var myAge = 10; 
+var earlyYears = 2;
+earlyYears *= 10.5; 
+var laterYears = myAge - 2;
+laterYears *= 4;  
+console.log(laterYears);
+console.log(earlyYears);
+var myAgeInDogYears =  laterYears + earlyYears;console.log(myAgeInDogYears);
+var myName = "Danila";
+console.log(myName.toLowerCase());
+console.log('My name is ' + myName +'. I am ' + myAge + ' years old in human years which is ' + laterYears + ' years old in dog years.');
